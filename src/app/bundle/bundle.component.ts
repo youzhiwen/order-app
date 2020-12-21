1838a2b9e6fbc20161eb107fe25e620df51ef8aa
fen <franwang82@gmail.com>
2020-12-21 20:54:49 +1300

    modify bundle using grids and try to make modal for back to order list

--- src/app/bundle/bundle.component.ts
+++ src/app/bundle/bundle.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({selector: 'app-bundle',templateUrl: './bundle.component.html'})
 export class BundleComponent implements OnInit {
   
   constructor(
-    private router: Router) {
+    private router: Router, private modalService: NgbModal) {
   }
   ngOnInit(): void {
   }
   
+  OnClickReturnModal(){
+    this.modalService.open; 
+  }
   returnToOrderList() {
-        this.router.navigate(['/home']);
+      this.router.navigate(['/home']);
     }
 }
