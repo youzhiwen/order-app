c76e9feb7ec0d2c875938a05d5a8132677884933
Stephen You <S.You@cbanewzealand.org.nz>
2020-12-21 20:58:40 +1300

    add ElementRef component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,22 +9,30 @@ import { Order } from '@app/_models';
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit {
 
+	@ViewChild('fulfilledOrder') fulfilledEl: ElementRef[];
+
 	orders: Order[] = [];
 	isOpenOrder = true;
 	checklist:any;
 	selectedOrderID:number;
+	selectedOrder: Order;
 
 	page = 1;
     count = 0;
     tableSize = 50;
 
-    constructor(private router: Router, private modalService: NgbModal, private el:ElementRef) {
+    constructor(private router: Router, private modalService: NgbModal) {
         
     }
 
     ngOnInit() {
         this.getOpenOrders();		
-    }
+	}
+	
+	clicked(order: Order){
+		this.selectedOrder = order;
+		console.log(this.selectedOrder)
+	}
    
 	checkUncheckAll() {
 		for (var i = 0; i < this.checklist.length; i++) {
@@ -70,7 +78,7 @@ export class HomeComponent implements OnInit {
 	cancelOrder(content){
 		this.modalService.open(content);
 		this.selectedOrderID = 1001;
-		//console.log(this.element.nativeElement)
+		console.log(this.fulfilledEl)
 	}
    
 	onTableDataChange(event){
